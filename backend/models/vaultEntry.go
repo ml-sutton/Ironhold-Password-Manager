321530144274423639173c239dbd2bc50d37b5f5
Madison Sutton <YNotMadisonUwU@gmail.com>
2026-05-18 12:05:26 +1000

    beginning work on go API

--- backend/models/vaultEntry.go
+++ backend/models/vaultEntry.go
@@ -5,6 +5,15 @@ type VaultEntry struct {
 	Blob []byte
 }
 
-func CreateVaultEntry() {
+func CreateVaultEntry(guid string, blob []byte) *VaultEntry {
+	var VaultEntryObject VaultEntry = VaultEntry{
+		GUID: guid,
+		Blob: blob,
+	}
+	return &VaultEntryObject
+}
 
+type CreateVaultEntryRequest struct {
+}
+type CreateVaultEntryHeaders struct {
 }
--- backend/routes/vault.go
+++ backend/routes/vault.go
@@ -1,1 +1,28 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ml-sutton/Ironhold-Password-Manager/backend/models"
+)
+
+func CreateVaultEntry(ctx *gin.Context) {
+	// Checks headers
+	var requestHeaders models.CreateVaultEntryHeaders
+	var requestHeaderBindError = ctx.ShouldBindHeader(&requestHeaders)
+	if nil != requestHeaderBindError {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"": "",
+		})
+	}
+
+	var requestBody models.CreateVaultEntryRequest
+	var requestBodyBindError = ctx.ShouldBindBodyWithJSON(&requestBody)
+	if nil != requestBodyBindError {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"": "",
+		})
+	}
+
+}
